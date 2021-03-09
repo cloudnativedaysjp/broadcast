5b0716c9c44ac851c5e01d0d684a455d8aa24f6b
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2021-03-10 02:19:22 +0900

    Create nginx-rtmp.sh

---
+++ nginx-rtmp/nginx-rtmp.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+
+# https://github.com/arut/nginx-rtmp-module
+
+set -xe
+
+export NGINX_VERSION=1.19.7
+
+apt-get install curl unzip build-essential libpcre3 libpcre3-dev libssl-dev zlib1g -y
+
+curl -LO http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz && tar xvf nginx-$NGINX_VERSION.tar.gz
+curl -LO https://github.com/sergey-dryabzhinsky/nginx-rtmp-module/archive/dev.zip && unzip dev.zip
+
+cd nginx-$NGINX_VERSION
+./configure --add-module=../nginx-rtmp-module-dev
+make
+make install
+
+cat << EOF > /usr/local/nginx/sbin/conf/nginx.conf
+user nginx;
+worker_processes  1;
+
+error_log  /usr/local/nginx/sbin/log/error.log warn;
+pid        /usr/local/nginx/sbin/log/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+rtmp{
+    server {
+        listen 1935;
+        chunk_size 4092;
+
+        application live {
+            live on;            
+        }
+    }
+    
+    server {
+        listen 31935;
+        chunk_size 4092;
+
+        application live {
+            live on;            
+        }
+    }
+}
+EOF
