6f2677d6b0b527a8ab66a8ca5d7a056669ffb081
TakumaNakagame(kameneko) <5129906+TakumaNakagame@users.noreply.github.com>
2022-02-26 01:50:50 +0900

    Update Install-script.ps1

--- sandbox/Install-script.ps1
+++ sandbox/Install-script.ps1
@@ -1,46 +1,47 @@
 Param($Args1,$Args2,$Args3,$Args4)
 
-echo "User is ${Args1}"
-
 
 ## IP Setting
+echo ">>> Start ip address setting"
 New-NetIPAddress -InterfaceIndex ${Args4} -AddressFamily IPv4 -IPAddress ${Args2} -PrefixLength 24 -DefaultGateway ${Args3}
 
+## Disable firewall
+echo ">>> Disable firewall"
+Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled false
+
 ## Cortana Uninstall
+echo ">>> Uninstall Cortana"
 Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
 
-## Windows Exporter
-Invoke-WebRequest https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-386.exe -UseBasicParsing -OutFile "C:\Users\${Args1}\Downloads\windows-exporter.exe"
-
-Copy-Item "C:\Users\${Args1}\Downloads\windows-exporter.exe" "C:\Users\${Args1}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
+## Setup Chocolatey
+echo ">>> setup chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # obs
-echo ">>> Start downloading OBS Studio in zip format"
-Invoke-WebRequest https://cdn-fastly.obsproject.com/downloads/OBS-Studio-27.1.3-Full-x64.zip -UseBasicParsing -OutFile "C:\Users\${Args1}\Downloads\OBS-Studio-27.1.3-Full-x64.zip"
-echo ">>> Unarchive a  zip file of OBS Studio"
-Unblock-File -Path "C:\Users\${Args1}\Downloads\OBS-Studio-27.1.3-Full-x64.zip"
-Expand-Archive -Path "C:\Users\${Args1}\Downloads\OBS-Studio-27.1.3-Full-x64.zip" -DestinationPath "C:\Users\${Args1}\Downloads\obs-studio"
-echo ">>> Copy Program files"
-Copy-Item C:\Users\${Args1}\Downloads\obs-studio 'C:\Program Files\' -Recurse
-## Create Shortcut
-echo ">>> Create shortcut"
-$WsShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WsShell.CreateShortcut("C:\Users\${Args1}\Desktop\OBS Studio.lnk")
-$Shortcut.TargetPath = 'C:\Program Files\obs-studio\bin\64bit\obs64.exe'
-$Shortcut.WorkingDirectory = 'C:\Program Files\obs-studio\bin\64bit\'
-$Shortcut.Save()
+echo ">>> Install OBS Studio"
+choco install obs-studio -y
 
+# install nextcloud-client
+echo ">>> install nextcloud-client"
+choco install nextcloud-client -y
+
+## Windows Exporter
+echo ">>> Setup windows exporter"
+Invoke-RestMethod -ContentType "application/octet-stream" -Uri https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-386.exe -OutFile "~\Downloads\windows-exporter.exe"
+
+Copy-Item "~\Downloads\windows-exporter.exe" "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Recurse
 
 # obs-ndi
 echo ">>> Start downloading obs-ndi in zip format"
-Invoke-WebRequest https://github.com/Palakis/obs-ndi/releases/download/4.9.1/obs-ndi-4.9.0-Windows.zip -UseBasicParsing -OutFile "C:\Users\${Args1}\Downloads\obs-ndi-4.9.0-Windows.zip"
+Invoke-RestMethod -ContentType "application/octet-stream" -Uri https://github.com/Palakis/obs-ndi/releases/download/4.9.1/obs-ndi-4.9.0-Windows.zip -OutFile "~\Downloads\obs-ndi-4.9.0-Windows.zip"
 echo ">>> Unarchive a  zip file of obs-ndi"
-Unblock-File -Path "C:\Users\${Args1}\Downloads\obs-ndi-4.9.0-Windows.zip"
-Expand-Archive -Path "C:\Users\${Args1}\Downloads\obs-ndi-4.9.0-Windows.zip" -DestinationPath "C:\Users\${Args1}\Downloads\obs-ndi"
+Unblock-File -Path "~\Downloads\obs-ndi-4.9.0-Windows.zip"
+Expand-Archive -Path "~\Downloads\obs-ndi-4.9.0-Windows.zip" -DestinationPath "~\Downloads\obs-ndi"
 echo ">>> Copy plugins to existing obs directory"
-Copy-Item C:\Users\${Args1}\Downloads\obs-ndi\data\obs-plugins\obs-ndi 'C:\Program Files\obs-studio\data\obs-plugins' -Recurse
-Copy-Item C:\Users\${Args1}\Downloads\obs-ndi\obs-plugins\64bit\* 'C:\Program Files\obs-studio\obs-plugins\64bit' -Recurse
+Copy-Item ~\Downloads\obs-ndi\data\obs-plugins\obs-ndi 'C:\Program Files\obs-studio\data\obs-plugins' -Recurse
+Copy-Item ~\Downloads\obs-ndi\obs-plugins\64bit\* 'C:\Program Files\obs-studio\obs-plugins\64bit' -Recurse
 
 # NDI-Tools
 echo ">>> Start downloading NDI-Tools in exe format"
-Invoke-WebRequest https://downloads.ndi.tv/Tools/NDI%205%20Tools.exe -UseBasicParsing -OutFile "C:\Users\${Args1}\Downloads\NDI-Tools.exe"
+Invoke-RestMethod -ContentType "application/octet-stream" -Uri https://downloads.ndi.tv/Tools/NDI%205%20Tools.exe -OutFile "~\Downloads\NDI-Tools.exe"
+~\Downloads\NDI-Tools.exe
