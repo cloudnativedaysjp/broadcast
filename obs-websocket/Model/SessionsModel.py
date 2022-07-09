cc0156ebcfbc47f60c4606d178f014f1c9541702
Gaku-Kunimi <dummy@uminek.ooo>
2022-07-10 01:55:57 +0900

    シーンのみ作るハードコード版を作成

--- obs-websocket/Model/SessionsModel.py
+++ obs-websocket/Model/SessionsModel.py
@@ -5,5 +5,8 @@ class SessionsModel:
         self.data = None
         self.filename = filename
         # TODO: ディレクトリ内を走査して トラック 毎の csv を読む
-        with open(filename, 'r') as f:
-            self.data = csv.DictReader(f)
+        self.f = open(filename, 'r')
+        self.data = csv.DictReader(self.f)
+    def __del__(self):
+        self.f.close() # これでいいのかは不明 with open だと インスタンスから .data にアクセスしたときに I/O operation on closed file. となる
+
--- obs-websocket/temp_scene_controller.py
+++ obs-websocket/temp_scene_controller.py
@@ -4,6 +4,10 @@ import asyncio
 import simpleobsws
 import os
 
+from Model.SceneTmplModel import SceneTmplModel
+from Model.SessionsModel import SessionsModel
+
+
 ### バッチリクエストのサンプルをもとに、一旦すべてハードコードで作る
 
 parameters = simpleobsws.IdentificationParameters(ignoreNonFatalRequestChecks = False) # Create an IdentificationParameters object (optional for connecting)
@@ -14,21 +18,44 @@ PASS = os.environ["WSPASS"]
 
 ws = simpleobsws.WebSocketClient(url = f'ws://{HOST}:{PORT}', password = PASS, identification_parameters = parameters) # Every possible argument has been passed, but none are required. See lib code for defaults.
 
-async def make_request():
-    await ws.connect() # Make the connection to obs-websocket
-    await ws.wait_until_identified() # Wait for the identification handshake to complete
+class TempSceneController:
+    def __init__(self, filename) -> None:
+        self.filename = filename
+        self.scenetmpl = SceneTmplModel()
+        self.sessionsmodel = SessionsModel(f"csv/{filename}.csv") # 仮
+        self.scenedata = None # TODO: ここで、 self.scenetmpl と self.sessions から作るべきシーンを dict に起こす
 
-    requests = []
+    async def create_scenes(self):
+        await ws.connect() # Make the connection to obs-websocket
+        await ws.wait_until_identified() # Wait for the identification handshake to complete
 
-    requests.append(simpleobsws.Request('CreateSceneCollection', {'sceneCollectionName': 'cnsec2022_2022-07-12_A'}))
+        requests = []
 
-    ret = await ws.call_batch(requests, halt_on_failure = False) # Perform the request batch
+        # profile もいじって FHD にする
 
-    for result in ret:
-        if result.ok(): # Check if the request succeeded
-            print("Request succeeded! Response data: {}".format(result.responseData))
+        requests.append(simpleobsws.Request('CreateSceneCollection', {'sceneCollectionName': f'{self.filename}'}))
+        requests.append(simpleobsws.Request('CreateScene', {'sceneName': '調整中'}))
+        requests.append(simpleobsws.Request('CreateScene', {'sceneName': '待機ループ'}))
+        for session in self.sessionsmodel.data:
+            name = f"{self.sessionsmodel.filename[-8:-4]}-{session['start_to_end']}-{session['title'][0:8]}"
+            requests.append(simpleobsws.Request('CreateScene', {'sceneName': f"{name}"}))
+        requests.append(simpleobsws.Request('CreateScene', {'sceneName': 'closing 前待機ループ'}))
+        requests.append(simpleobsws.Request('CreateScene', {'sceneName': 'closing'}))
 
-    await ws.disconnect() # Disconnect from the websocket server cleanly
+        # ソース
+
+        ret = await ws.call_batch(requests, halt_on_failure = False) # Perform the request batch
+
+        for result in ret:
+            if result.ok(): # Check if the request succeeded
+                print("Request succeeded! Response data: {}".format(result.responseData))
+            else:
+                print(f" {result.requestType} Request Failed! 詳細: {result.requestStatus.comment}")
+
+        await ws.disconnect() # Disconnect from the websocket server cleanly
+
+controller = TempSceneController('cnsec2022_2022-07-12_A')
+# controller = TempSceneController('cnsec2022_2022-07-12_B')
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(make_request())
+loop.run_until_complete(controller.create_scenes())
