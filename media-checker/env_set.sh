54cb3ecf1c5422f2f2af400dea3791ff499a3fc5
tanayan299 <infuse130@gmail.com>
2022-07-21 12:09:55 +0900

    Modify sample env set

--- media-checker/env_set.sh
+++ media-checker/env_set.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cat <<EOS > <PATH TO ENV FILE>
 AUTH0_DOMAIN=dreamkast.us.auth0.com
 CLIENT_ID=<CLIENT ID>
 CLIENT_SECRET=<CLIENT_SECRET>
@@ -10,5 +9,13 @@ TOKEN=$(curl --url https://${AUTH0_DOMAIN}/oauth/token \
   --data "{\"client_id\":\"${CLIENT_ID}\",\"client_secret\":\"${CLIENT_SECRET}\",\"audience\":\"${AUDIENCE}\",\"grant_type\":\"client_credentials\"}" | jq -r .access_token)
 DREAMKAST_DOMAIN='event.cloudnativedays.jp'
 SLACKURL=<SLACK WEBHOOK URL>
-EOS
 
+cat <<EOS > <PATH TO ENV FILE>
+AUTH0_DOMAIN=${AUTH0_DOMAIN}
+CLIENT_ID=${CLIENT_ID}
+CLIENT_SECRET=${CLIENT_SECRET}
+AUDIENCE=${AUDIENCE}
+TOKEN=${TOKEN}
+DREAMKAST_DOMAIN=${DREAMKAST_DOMAIN}
+SLACKURL=${SLACKURL}
+EOS
