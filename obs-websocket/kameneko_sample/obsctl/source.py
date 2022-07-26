48ac7aaf31b1bd0b0146fd8ba70819bf274a2a3b
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2022-07-26 19:36:13 +0900

    :fire: remove obsctl

--- obs-websocket/kameneko_sample/obsctl/source.py
+++
@@ -1,9 +0,0 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
-import asyncio
-import simpleobsws
-import os
-import sys
-# obsctl get source {sceneName}
-async def get(ws, sceneName):
-    logging.debug("get_source({})".format(sceneName))
