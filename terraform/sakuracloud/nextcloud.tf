a161b5a7b59ba808350716b8afb7285bb3105630
Kazuto Kusama <jacopen@gmail.com>
2022-08-02 15:58:37 +0900

    Update nextcloud.tf

--- terraform/sakuracloud/nextcloud.tf
+++ terraform/sakuracloud/nextcloud.tf
@@ -57,8 +57,8 @@ resource "sakuracloud_server" "nextcloud" {
     sakuracloud_disk.nextcloud_data.id,
     sakuracloud_disk.nextcloud_backup.id
   ]
-  core        = 4
-  memory      = 8
+  core        = 2
+  memory      = 4
   description = "Nextcloud server"
   tags        = ["app=nextcloud", "stage=production", "starred"]
 
