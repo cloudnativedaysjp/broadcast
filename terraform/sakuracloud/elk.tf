33b79132c5a8dd4c1350547e5a56043d8d0cc4b8
Kazuto Kusama <jacopen@gmail.com>
2022-09-29 20:43:57 +0900

    Add ELK VM

---
+++ terraform/sakuracloud/elk.tf
@@ -0,0 +1,44 @@
+resource "sakuracloud_disk" "elk" {
+	name              = "elk"
+  source_archive_id = data.sakuracloud_archive.ubuntu.id
+  plan              = "ssd"
+  connector         = "virtio"
+  size              = 512
+
+  lifecycle {
+    ignore_changes = [
+      source_archive_id,
+    ]
+  }
+}
+
+resource "sakuracloud_server" "elk" {
+  name = "elk"
+  disks = [
+    sakuracloud_disk.elk.id,
+  ]
+  core        = 4
+  memory      = 8
+  description = "ELK Stack"
+  tags        = ["app=elk", "stage=production", "starred"]
+
+  network_interface {
+    upstream = "shared"
+    packet_filter_id = sakuracloud_packet_filter.nextcloud.id
+  }
+
+  network_interface {
+    upstream = data.sakuracloud_switch.switcher.id
+  }
+
+  user_data = templatefile("./template/cloud-init.yaml", {
+    vm_password = random_password.password.result,
+    hostname    = "elk"
+  })
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
